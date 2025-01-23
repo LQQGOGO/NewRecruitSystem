361e8b129fcd49f3de977644c402284a5267fc7b
Eddie <Eddie_yang7@163.com>
2025-01-23 17:48:13 +0800

    完成了侧边栏的样式

--- src/views/layout/IosPage.vue
+++ src/views/layout/IosPage.vue
@@ -1,9 +1,71 @@
 <script setup>
-import MouseComponent from '@/components/MouseComponent.vue';
+import MouseComponent from '@/components/MouseComponent.vue'
+import TextComponent from '@/components/TextComponent.vue'
+import ArrowComponent from '@/components/ArrowComponent.vue'
 </script>
 
 <template>
-  <div>
+  <div class="container">
+    <!-- Ios组介绍部分 -->
+    <div class="introduction">
+      <TextComponent>
+        <template #title>
+          <div>Ios组简介</div>
+        </template>
+        <template #text>
+          <p>
+            iOS 开发组现有成员12个。分别来自计科、软件、网络等专业。iOS组配备了
+            15 台 Mac mini 和 7 台
+            iMac。是目前实验室设备配置最好，规模最大的开发组。
+            组成员们利用实验室设备和iOS平台的种种优势，积极进取，共同奋斗。先后开发了
+            《iXiyou》、《西邮图书馆》、《快递查询》、《生活百事通》、
+            《微博客户端》、《天气查询系统》等移动应用,并在不断的完善、优化。
+          </p>
+        </template>
+      </TextComponent>
+    </div>
+
+    <!-- 箭头部分 -->
+    <ArrowComponent>
+      <h1>点击下方按钮即可报名</h1>
+    </ArrowComponent>
+
+    <!-- 屏幕底部仓鼠部分 -->
     <MouseComponent></MouseComponent>
+
+    <!-- 装饰栏部分 -->
+    <div>
+      <!-- 左边装饰部分 -->
+      <div class="left"></div>
+
+      <!-- 右边装饰部分 -->
+      <div class="right"></div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50rem;
+  margin: 0 auto;
+}
+
+.left {
+  float: left;
+  height: 50rem;
+  width: 25rem;
+  background-color: #fff;
+  transform: translate3d(-40rem, -25rem, 0);
+}
+
+.right {
+  float: right;
+  height: 50rem;
+  width: 25rem;
+  background-color: #d42525;
+  transform: translate3d(40rem, -25rem, 0);
+}
+</style>
