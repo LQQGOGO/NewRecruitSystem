be4278b11031a033c449c4e99f2cccde3f6d0c40
Eddie <Eddie_yang7@163.com>
2025-01-25 14:16:39 +0800

    完成了字体彩虹效果

--- src/components/TagLine.vue
+++ src/components/TagLine.vue
@@ -1,18 +1,5 @@
-<template>
-  <div class="tagline">
-    <span
-      v-for="(char, index) in text"
-      :key="index"
-      class="splitted-char"
-      :style="{ transform: `translateX(${index % 2 === 0 ? '-40%' : '40%'}) skewX(${index % 2 === 0 ? '10deg' : '-10deg'})` }"
-    >
-      {{ char }}
-    </span>
-  </div>
-</template>
-
 <script setup>
-import { computed } from 'vue';
+import { computed } from 'vue'
 
 // 接收父组件传递的文本内容
 const props = defineProps({
@@ -20,28 +7,60 @@ const props = defineProps({
     type: String,
     default: ''
   }
-});
+})
 
 // 将传入的文本内容分割成单个字符数组
-const text = computed(() => props.content.split(''));
+const text = computed(() => props.content.split(''))
 </script>
+
+<template>
+  <div class="tagline">
+    <span
+      v-for="(char, index) in text"
+      :key="index"
+      class="splitted-char"
+      :style="{
+        transform: `translateX(${index % 2 === 0 ? '-40%' : '40%'}) skewX(${index % 2 === 0 ? '10deg' : '-10deg'})`,
+        transitionDelay: `${index * 0.1}s`
+      }"
+    >
+      {{ char }}
+    </span>
+  </div>
+</template>
 
 <style scoped>
 .tagline {
   display: flex;
   flex-direction: column;
   align-items: center;
-  font-size: 4rem; /* 增大字体大小 */
-  line-height: 1.2; /* 调整行高 */
+  font-size: 4rem;
+  line-height: 1.2;
 }
 
 .splitted-char {
   color: transparent;
-  background-image: linear-gradient(45deg, rgb(111, 111, 208), rgb(46, 187, 177));
+  background-image: linear-gradient(
+    45deg,
+    rgb(111, 111, 208),
+    rgb(46, 187, 177)
+  );
   -webkit-background-clip: text;
   background-clip: text;
   display: inline-block;
   position: relative;
-  margin: 0.5rem 0; /* 增加垂直间距 */
+  margin: 0.5rem 0;
+  -webkit-text-stroke: 2px #a481e2;
+  text-shadow: 1px 1px 12px rgb(230, 207, 254);
+  transition:
+    font-weight 1s ease,
+    font-size 1s ease; /* 添加过渡效果 */
+}
+
+.tagline:hover .splitted-char {
+  transform: scale(1.1); /* 鼠标经过时放大 */
+  font-weight: bold; /* 鼠标经过时字体加粗 */
+  border-radius: 5px; /* 增加圆润度 */
+  font-variation-settings: "BEVL" 100;
 }
 </style>
